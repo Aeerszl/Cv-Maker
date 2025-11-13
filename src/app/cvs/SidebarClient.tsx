43317b63529f20712964c394f7407eb6e6c44d81
Ali.E.E <aeerszl@gmail.com>
2025-11-13 17:06:15 +0300

    fix: Add TypeScript types to SidebarClient component
    - Fix implicit any type error for userName and userEmail parameters
    - Add proper interface for SidebarClientProps

--- src/app/cvs/SidebarClient.tsx
+++ src/app/cvs/SidebarClient.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { Sidebar } from '@/components/dashboard/Sidebar';
 
-export default function SidebarClient({ userName, userEmail }) {
+interface SidebarClientProps {
+  userName?: string;
+  userEmail?: string;
+}
+
+export default function SidebarClient({ userName, userEmail }: SidebarClientProps) {
   return <Sidebar userName={userName} userEmail={userEmail} />;
 }
